9bf35303ded403bc344c85c103dbfe695c3b3e4b
srush013 <srush013@gmail.com>
2014-09-06 16:13:15 -0700

    Basic style changes to week display

--- week.php
+++ week.php
@@ -9,7 +9,9 @@ if(!isset($_GET['week'])) {
 $week = pg_escape_string($_GET['week']);
 $year = isset($_GET['year']) ? pg_escape_string($_GET['year']) : 2014;
 
-echo "<html><head><title>BQBL Week $week $year</title></head>\n";
+echo "<html><head>
+<title>BQBL Week $week $year</title></head><body>\n
+<h1>Week $week $year Scoreboard</h1>";
 
 $query = "SELECT gsis_id, home_team, away_team
 		  FROM game
@@ -20,13 +22,24 @@ echo "<div style='display:table;'>";
 while(list($gsis,$hometeam,$awayteam) = pg_fetch_array($result)) {
     $gameType = gameTypeById($gsis);
     echo "<div id=matchup style='display:table-row;'>\n";
-    echo "<div id=score style='display:table-cell;'>\n";
+    echo "<div class=score>\n";
     echo "$hometeam\n";
     printGameScore($awayteam, $week, $year);
-    echo "</div><div style='display:table-cell;vertical-align:middle;'>AT</div>\n";
+    echo "</div><div class=score>@</div>\n";
+    echo "<div class=score >\n";
     echo "$awayteam\n";
-    echo "<div id=score style='style='display:table-cell;'>\n";
     printGameScore($hometeam, $week, $year);
     echo"</div></div>";
 }
 echo "</div>";
+
+?>
+<style>
+.score {
+font-size: x-large;
+font-weight:bold;
+text-align: center;
+display:table-cell;
+padding:0 20px 60px 0;
+}
+</style>
