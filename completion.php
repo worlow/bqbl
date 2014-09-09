7cf9cb76fffc02ac95a6a9e71472e8b2103eafa0
Stephen Worlow <stevo8791@gmail.com>
2014-09-08 21:10:30 -0700

    Completion stuff

--- completion.php
+++ completion.php
@@ -7,7 +7,10 @@ $query = "SELECT gsis_id, home_team, away_team
 		  WHERE season_year='2013' AND season_type='Regular'
           ORDER BY start_time ASC;";
 $result = pg_query($query);
+$tot = 0;
 while(list($gsis,$hometeam,$awayteam) = pg_fetch_array($result)) {
-	echo $hometeam + completionPct($gsis, $hometeam);
-        echo $awayteam + completionPct($gsis, $awayteam);
+        $tot++;
+	echo $hometeam + number_format(completionPct($gsis, $hometeam),1) + "\n";
+        echo $awayteam + number_format(completionPct($gsis, $awayteam),1) +  "\n";
 }
+echo $tot;
