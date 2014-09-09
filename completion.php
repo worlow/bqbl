be3f9286e8998ff8b3ea6b19b645ca656ebd4199
Stephen Worlow <stevo8791@gmail.com>
2014-09-09 08:20:31 -0700

    Print all games under 55%

--- completion.php
+++ completion.php
@@ -20,12 +20,10 @@ $tot75 = 0;
 $tot80 = 0;
 while(list($gsis,$hometeam,$awayteam) = pg_fetch_array($result)) {
 	if (number_format(completionPct($gsis, $hometeam),1) <= 55) {	
-		echo "Completion Percentage:".number_format(completionPct($gsis, $hometeam),1).", Passing TDs:".passingTDs($gsis, $hometeam).", Yards:".passingYards($gsis, $hometeam)."Interceptions:".ints($gsis,$hometeam);
-		echo "<br>\n";
+		echo "Completion Percentage:".number_format(completionPct($gsis, $hometeam),1).", Passing TDs:".passingTDs($gsis, $hometeam).", Yards:".passingYards($gsis, $hometeam).", Interceptions:".ints($gsis,$hometeam)."<br>\n";
 	}
 	if (number_format(completionPct($gsis, $awayteam),1) <= 55) {
-		echo "Completion Percentage:".number_format(completionPct($gsis, $awayteam),1).", Passing TDs:".passingTDs($gsis, $awayteam).", Yards:".passingYards($gsis, $awayteam)."Interceptions:".ints($gsis,$awayteam);
-		echo "<br>\n";
+		echo "Completion Percentage:".number_format(completionPct($gsis, $awayteam),1).", Passing TDs:".passingTDs($gsis, $awayteam).", Yards:".passingYards($gsis, $awayteam).", Interceptions:".ints($gsis,$awayteam)."<br>\n";
 	}
 	if (number_format(completionPct($gsis, $hometeam),1) <= 35) {
 		$tot35++;
@@ -74,15 +72,15 @@ while(list($gsis,$hometeam,$awayteam) = pg_fetch_array($result)) {
 		$tot++;
 	}
 }
-echo "Less than 35 $tot35 \n";
-echo "Less than 40 $tot40 \n";
-echo "Less than 45 $tot45 \n";
-echo "Less than 50 $tot50 \n";
-echo "Less than 55 $tot55 \n";
-echo "Less than 60 $tot60 \n";
-echo "Less than 65 $tot65 \n";
-echo "Less than 70 $tot70 \n";
-echo "Less than 75 $tot75 \n";
-echo "Less than 80 $tot80 \n";
-echo "More than 80 $tot \n";
+echo "Less than 35 $tot35 <br>\n";
+echo "Less than 40 $tot40 <br>\n";
+echo "Less than 45 $tot45 <br>\n";
+echo "Less than 50 $tot50 <br>\n";
+echo "Less than 55 $tot55 <br>\n";
+echo "Less than 60 $tot60 <br>\n";
+echo "Less than 65 $tot65 <br>\n";
+echo "Less than 70 $tot70 <br>\n";
+echo "Less than 75 $tot75 <br>\n";
+echo "Less than 80 $tot80 <br>\n";
+echo "More than 80 $tot <br>\n";
 
