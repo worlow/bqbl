3eaddd07fe0f4184c22e23d48d75c873e493e383
Stephen Worlow <stevo8791@gmail.com>
2014-10-11 00:03:03 -0700

    Put back gwd

--- leaderboard.php
+++ leaderboard.php
@@ -145,9 +145,9 @@ function totalScore($team, $week, $year=2014, $isOffense) {
         elseif($completionPct < 50) $points['completionPct'] = 5;
     $points['safeties'] = 20*$safeties;
     $points['overtimeTaints'] = 50*$overtimeTaints;
+    $points['gameWinningDrive'] = -12*$gameWinningDrive;
     if ($isOffense) {
         $points['benchings'] = 35*$benchings;
-        $points['gameWinningDrive'] = -12*$gameWinningDrive;
         $points['miscPoints'] = $miscPoints;
     }
     
