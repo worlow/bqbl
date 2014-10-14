aadb8559cbaacf663e69b621a203baa3db167cfe
Stephen Worlow <stevo8791@gmail.com>
2014-10-14 02:22:59 -0700

    colors

--- schedule.php
+++ schedule.php
@@ -37,9 +37,9 @@ for ($i = 1; $i <= 14; $i++) {
     echo "<tr><td>Week $i</td>";
     for ($j = 1; $j <= 8; $j++) {
         if ($score[$j][$i] > $score[$matchup[$i][$j]][$i]) {
-            echo "<td background-color='green'>".$bqbl_teamname[$matchup[$i][$j]]."</td>";
+            echo "<td color='green'>".$bqbl_teamname[$matchup[$i][$j]]."</td>";
         } elseif ($score[$j][$i] < $score[$matchup[$i][$j]][$i]) {
-            echo "<td background-color='red'>".$bqbl_teamname[$matchup[$i][$j]]."</td>";
+            echo "<td color='red'>".$bqbl_teamname[$matchup[$i][$j]]."</td>";
         } else {
             echo "<td>".$bqbl_teamname[$matchup[$i][$j]]."</td>";
         }
