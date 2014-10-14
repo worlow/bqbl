2a1af18675407d4191a704659363ecdc406dd76d
Stephen Worlow <stevo8791@gmail.com>
2014-10-13 23:33:52 -0700

    Bqbl_team not id

--- matchup.php
+++ matchup.php
@@ -24,8 +24,8 @@ $query = "SELECT bqbl_team, starter1, starter2
               WHERE year = $year AND week = $week;";
 $result = pg_query($bqbldbconn, $query);
 while(list($bqbl_team,$starter1,$starter2) = pg_fetch_array($result)) {
-    $lineup[$id][0] = $starter1;
-    $lineup[$id][1] = $starter2;
+    $lineup[$bqbl_team][0] = $starter1;
+    $lineup[$bqbl_team][1] = $starter2;
 }
 
 $query = "SELECT team1, team2
