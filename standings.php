66c212b8670b2b8b57b7582c7d485db6ce4becd6
Stephen Worlow <stevo8791@gmail.com>
2014-10-14 22:27:29 -0700

    streak

--- standings.php
+++ standings.php
@@ -18,14 +18,12 @@ $bqbl_teamname = bqblTeams();
 $matchup = array();
 $record = array();
 $score = array();
-$points_for = array();
-$points_against = array();
 foreach ($bqbl_teamname as $key => $val) {
     $record[$key]['wins'] = 0;
     $record[$key]['losses'] = 0;
     $record[$key]['points_for'] = 0;
-    $points_for[$key] = 0;
-    $points_against[$key] = 0;
+    $record[$key]['points_against'] = 0;
+    $record[$key]['streak'] = 0;
 }
 
 for ($i = 1; $i <= $week; $i++) {
@@ -38,31 +36,54 @@ for ($i = 1; $i <= $week; $i++) {
     $matchup = getMatchups($year, $i);
     foreach ($matchup as $team1 => $team2) {
         $record[$team1]['points_for'] += $score[$team1][$i];
-        $points_against[$team1] += $score[$team2][$i];
+        $record[$team1]['points_against'] += $score[$team2][$i];
         
         $record[$team2]['points_for'] += $score[$team2][$i];
-        $points_against[$team2] += $score[$team1][$i];
+        $record[$team2]['points_against'] += $score[$team1][$i];
         if ($score[$team1][$i] > $score[$team2][$i]) {
             $record[$team1]['wins']++;
             $record[$team2]['losses']++;
+            
+            if ($record[$team1]['streak'] < 0) {
+                $record[$team1]['streak'] = 1;
+            } else {
+                $record[$team1]['streak']++;
+            }
+            if ($record[$team2]['streak'] > 0) {
+                $record[$team2]['streak'] = -1;
+            } else {
+                $record[$team2]['streak']--;
+            }
         } elseif ($score[$team1][$i] < $score[$team2][$i]) {
             $record[$team1]['losses']++;
             $record[$team2]['wins']++;
+            
+            if ($record[$team1]['streak'] > 0) {
+                $record[$team1]['streak'] = -1;
+            } else {
+                $record[$team1]['streak']--;
+            }
+            if ($record[$team2]['streak'] < 0) {
+                $record[$team2]['streak'] = 1;
+            } else {
+                $record[$team2]['streak']++;
+            }
         }
     }
 }
 
 arsort($record);
 echo '<table border=2 cellpadding=4 style="border-collapse:collapse;display:inline-block;">';
-echo "<tr><th></th><th>Team</th><th>W</th><th>L</th><th>PF</th><th>PA</th><th>PD</th></tr>";
+echo "<tr><th></th><th>Team</th><th>Wins</th><th>Losses</th><th>Points For</th>
+    <th>Points Against</th><th>Point Differential</th><th>Streak</th></tr>";
 $rank = 0;
 foreach ($record as $key => $val) {
     $rank++;
-    $point_differential = $val['points_for'] - $points_against[$key];
+    $point_differential = $val['points_for'] - $val['points_against'];
     $thickline = ($rank==4) ? "class='thickline'" : "";
     echo "<tr $thickline><td>$rank.</td><td>$bqbl_teamname[$key]</td><td>".$val['wins']."</td>
-        <td>".$val['losses']."</td><td>".$val['points_for']."</td><td>$points_against[$key]</td>
-        <td>$point_differential</td></tr>";
+        <td>".$val['losses']."</td><td>".$val['points_for']."</td><td>".$val['points_against']."</td>
+        <td>$point_differential</td><td>".$val['streak']."</td></tr>";
 }
 echo "</table>";
 
