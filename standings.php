a40afb8e0a4d6505818179b5218e3ed450300cc0
Stephen Worlow <stevo8791@gmail.com>
2014-10-14 22:19:18 -0700

    More relelvant

--- standings.php
+++ standings.php
@@ -21,9 +21,9 @@ $score = array();
 $points_for = array();
 $points_against = array();
 foreach ($bqbl_teamname as $key => $val) {
-    $record[$key][0] = 0;
-    $record[$key][1] = 0;
-    $record[$key][2] = 0;
+    $record[$key]['wins'] = 0;
+    $record[$key]['losses'] = 0;
+    $record[$key]['points_for'] = 0;
     $points_for[$key] = 0;
     $points_against[$key] = 0;
 }
@@ -37,17 +37,17 @@ for ($i = 1; $i <= $week; $i++) {
     
     $matchup = getMatchups($year, $i);
     foreach ($matchup as $team1 => $team2) {
-        $record[$team1][2] += $score[$team1][$i];
+        $record[$team1]['points_for'] += $score[$team1][$i];
         $points_against[$team1] += $score[$team2][$i];
         
-        $record[$team2][2] += $score[$team2][$i];
+        $record[$team2]['points_for'] += $score[$team2][$i];
         $points_against[$team2] += $score[$team1][$i];
         if ($score[$team1][$i] > $score[$team2][$i]) {
-            $record[$team1][0]++;
-            $record[$team2][1]++;
+            $record[$team1]['wins']++;
+            $record[$team2]['losses']++;
         } elseif ($score[$team1][$i] < $score[$team2][$i]) {
-            $record[$team1][1]++;
-            $record[$team2][0]++;
+            $record[$team1]['losses']++;
+            $record[$team2]['wins']++;
         }
     }
 }
@@ -58,9 +58,11 @@ echo "<tr><th></th><th>Team</th><th>W</th><th>L</th><th>PF</th><th>PA</th><th>PD
 $rank = 0;
 foreach ($record as $key => $val) {
     $rank++;
-    $point_differential = $val[2] - $points_against[$key];
+    $point_differential = $val['points_for'] - $points_against[$key];
     $thickline = ($rank==4) ? "class='thickline'" : "";
-    echo "<tr $thickline><td>$rank.</td><td>$bqbl_teamname[$key]</td><td>$val[0]</td><td>$val[1]</td><td>$val[2]</td><td>$points_against[$key]</td><td>$point_differential</td></tr>";
+    echo "<tr $thickline><td>$rank.</td><td>$bqbl_teamname[$key]</td><td>".$val['wins']."</td>
+        <td>".$val['losses']."</td><td>".$val['points_for']."</td><td>$points_against[$key]</td>
+        <td>$point_differential</td></tr>";
 }
 echo "</table>";
 
