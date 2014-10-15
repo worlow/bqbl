93db42e26dae45c0d6b95ba233172188ea43ba42
Stephen Worlow <stevo8791@gmail.com>
2014-10-14 23:21:38 -0700

    Averages

--- seasonleaderboard.php
+++ seasonleaderboard.php
@@ -3,7 +3,7 @@
 require_once "lib.php";
 require_once "scoring.php";
 
-$week = isset($_GET['week']) ? pg_escape_string($_GET['week']) : currentWeek();
+$week = isset($_GET['week']) ? pg_escape_string($_GET['week']) : currentCompletedWeek();
 $year = isset($_GET['year']) ? pg_escape_string($_GET['year']) : currentYear();
 
 echo "<html><head>
@@ -11,28 +11,23 @@ echo "<html><head>
 
 $grandtotals = array();
 $grandtotals_defense = array();
+$starts = array();
 $owner = array();
 $bqbl_draftscore = array();
 $bqbl_teamname = bqblTeams();
 $nfl_draftscore = array();
 $draft_pick = array();
+$average = arra
 echo "<br><h1>$year Season Rankings</h1>";
 
 foreach (nflTeams() as $team) {
+    $starts[$team] = 0;
     $grandtotals[$team] = 0;
     $grandtotals_defense[$team] = 0;
 }
 
 foreach ($bqbl_teamname as $key => $val) {
     $bqbl_draftscore[$key] = 0;
-}
-
-$query = "SELECT bqbl_team, nfl_team, draft_position
-    FROM roster;";
-$result = pg_query($GLOBALS['bqbldbconn'],$query);
-while(list($bqbl_team,$nfl_team,$draft_position) = pg_fetch_array($result)) {
-    $draft_pick[$nfl_team] = $draft_position;
-    $owner[$nfl_team] = $bqbl_team; 
 }
 
 for ($i=1; $i<=$week; $i++) {
@@ -42,6 +37,8 @@ for ($i=1; $i<=$week; $i++) {
           ORDER BY start_time ASC;";
     $result = pg_query($GLOBALS['nfldbconn'],$query);
     while(list($gsis,$hometeam,$awayteam) = pg_fetch_array($result)) {
+        $starts[$hometeam]++;
+        $starts[$awayteam]++;
         $homepoints = getPoints($hometeam, $i, $year);
         $awaypoints = getPoints($awayteam, $i, $year);
         $grandtotals[$hometeam] += totalPoints($homepoints);
@@ -50,26 +47,36 @@ for ($i=1; $i<=$week; $i++) {
         $grandtotals_defense[$awayteam] += defenseScore($homepoints);
     }
 }
-arsort($grandtotals);
-arsort($grandtotals_defense);
+
+$query = "SELECT bqbl_team, nfl_team, draft_position
+    FROM roster;";
+$result = pg_query($GLOBALS['bqbldbconn'],$query);
+while(list($bqbl_team,$nfl_team,$draft_position) = pg_fetch_array($result)) {
+    $draft_pick[$nfl_team] = $draft_position;
+    $owner[$nfl_team] = $bqbl_team; 
+    $average[$nfl_team] = $grandtotals[$nfl_team]/$starts[$nfl_team];
+    $average_defense[$nfl_team] = $grandtotals_defense[$nfl_team]/$starts[$nfl_team];
+}
+arsort($average);
+arsort($average_defense);
 
 echo '<table border=2 cellpadding=4 style="border-collapse:collapse;display:inline-block;">';
-echo "<tr><th>Rank</th><th>Team</th><th>Points</th></tr>";
+echo "<tr><th>Rank</th><th>Team</th><th>Points</th><th>Average</th></tr>";
 $rank = 0;
-foreach ($grandtotals as $key => $val) {
+foreach ($average as $key => $val) {
     $rank++;
     $nfl_draftscore[$key] = $draft_pick[$key] - $rank;
     $bqbl_draftscore[$owner[$key]] += $rank;
-    echo "<tr><td>$rank</td><td>$key</td><td>$val</td></tr>";
+    echo "<tr><td>$rank</td><td>$key</td><td>$grandtotals[$key]</td><td>$val</td></tr>";
 }
 echo "</table>";
 
 echo '<table border=2 cellpadding=4 style="border-collapse:collapse;display:inline-block; margin-left:20px;">';
-echo "<tr><th>Rank</th><th>Team</th><th>Defensive Points</th></tr>";
+echo "<tr><th>Rank</th><th>Team</th><th>Defensive Points</th><th>Average</th></tr>";
 $rank = 0;
-foreach ($grandtotals_defense as $key => $val) {
+foreach ($average_defense as $key => $val) {
     $rank++;
-    echo "<tr><td>$rank</td><td>$key</td><td>$val</td></tr>";
+    echo "<tr><td>$rank</td><td>$key</td><td>$grandtotals_defense[$key]</td><td>$val</td></tr>";
 }
 echo "</table>";
 
