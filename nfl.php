70f7b9832850ad1b08626a505e3d82d9d8306e54
Stephen Worlow <stevo8791@gmail.com>
2014-10-21 01:01:03 -0700

    Teams

--- nfl.php
+++ nfl.php
@@ -9,21 +9,25 @@ if ($nfl_team == null) {
        echo "<div><a href='/bqbl/nfl.php?team=$team&year=$year'>$team</a></div>";
    }
 } else {
+    $total = 0;
     echo '<table border=2 cellpadding=4 style="border-collapse:collapse;display:inline-block; margin-left:20px;">';
     echo "<tr><th></th><th>Opponent</th><th>Score</th></tr>";
     for ($i = 1; $i <= 17; $i++) {
         echo "<tr><td>Week $i</td>";
         
         list($home_team,$away_team) = nflMatchup($year, $i, $nfl_team);
-        echo $home_team;
         if ($home_team == $nfl_team) {
             echo "<td>".$away_team."</td>";
-        } else {
+        } elseif ($away_team == $nfl_team) {
             echo "<td>".$home_team."</td>";
+        } else {
+            echo "<td>BYE</td>";
         }
-        
-        echo "<td>".totalPoints(getPoints($nfl_team, $i, $year))."</td></tr>";
+        $points = totalPoints(getPoints($nfl_team, $i, $year));
+        $total += $points;
+        echo "<td>$points</td></tr>";
     }
+    echo "<tr><td>Total</td><td> -- </td><td>$points</td></tr>";
     echo "</table>";
 }
 ?>
